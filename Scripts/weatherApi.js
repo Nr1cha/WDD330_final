d66c3506b558ed8c3369c4654f97b71a454b9147
Nr1cha <vrm.richards@gmail.com>
2023-03-15 15:05:41 -0600

    added function call in index removed white space and commented items

--- Scripts/weatherApi.js
+++ Scripts/weatherApi.js
@@ -3,8 +3,8 @@
 //https://openweathermap.org/current#name
 // API URL
 const url =
-    "https://api.openweathermap.org/data/2.5/weather?lat=41.034878&lon=-111.938644&units=imperial&appid=72c90fbdfbdb409e818bc324052dfcd3"; //WDD330 API key 
-const page = document.querySelector("");
+    "https://api.openweathermap.org/data/2.5/weather?lat=41.034878&lon=-111.938644&units=imperial&appid=72c90fbdfbdb409e818bc324052dfcd3"; //WDD330 API key
+// const page = document.querySelector("");
 
 async function apiFetch() {
     try {
@@ -42,10 +42,6 @@ function displayResults(weatherdata) {
     weatherContainer.appendChild(currentTemp);
     weatherContainer.appendChild(windSpeed);
     weatherContainer.appendChild(captionDesc);
-
-
-
-
 
     // const windChill = document.querySelector("wind-chill");
 
