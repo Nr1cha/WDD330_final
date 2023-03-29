61877d353643ca85711ce061a1d3160fc0d338ac
Nr1cha <vrm.richards@gmail.com>
2023-03-28 20:08:01 -0600

    adding more for localstorage

--- Scripts/localStorage.js
+++ Scripts/localStorage.js
@@ -2,26 +2,28 @@ let locationsList = [];
 
 // GETTING ITEMS FROM THE PAGE
 
-
 // city text field
 const locationSearch = document.querySelector("#searchBox");
 
 // favorites button
 const favButton = document.querySelector("#addToFav");
 
+favButton.addEventListener("click", function () {
+    // get the value of the item typed
+    let searchTerm = locationSearch.value;
+    // Add the search term to the search history array
+    locationsList.push(searchTerm);
+});
+
 // list of favorites that are from localStorage
 const favoritesList = document.querySelector("#favList");
 
-
-
 // ADDING THINGS TO LOCAL STORAGE
-
 
 // store the city in text field to local storage
 localStorage.setItem("cityLocation", JSON.stringify(locationsList));
 
 // GETTING THINGS FROM LOCAL STORAGE
-
 
 // get the stored value in localStorage
 let listItems = JSON.parse(localStorage.getItem("cityLocation"));
