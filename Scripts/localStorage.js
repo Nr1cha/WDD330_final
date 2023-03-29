c8a746eaa65b5b03eee2e2b30c3e44ce23910e05
Nr1cha <vrm.richards@gmail.com>
2023-03-28 19:38:11 -0600

    setting up base for favorites button

--- Scripts/localStorage.js
+++ Scripts/localStorage.js
@@ -1,8 +1,3 @@
-// initialize display elements
-const todayDisplayDate = document.querySelector("");
-const visitsDisplay = document.querySelector("");
-const latestVisit = document.querySelector("");
-
 // get items from weather page
 const searchFavorite = document.querySelector("#addToFav");
 const favoritesList = document.querySelector("#favList");
@@ -10,19 +5,8 @@ const favoritesList = document.querySelector("#favList");
 // get the stored value in localStorage
 let numVisits = Number(window.localStorage.getItem(""));
 
-// last visit timestamp
-let lastVisit = Number(window.localStorage.getItem(""));
-
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `This is your first visit!`;
-}
-
-// increment the number of visits.
-numVisits++;
 // store the new number of visits value
 localStorage.setItem("", numVisits);
+
 // show todays date.
-todayDisplayDate.textContent = Date.now();
+// todayDisplayDate.textContent = Date.now();
