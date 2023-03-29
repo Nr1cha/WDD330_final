9ed4f4bd91fe5a3abc60f26f36339332c846e990
Nr1cha <vrm.richards@gmail.com>
2023-03-28 19:58:02 -0600

    adding more stuff for localstorage

--- Scripts/localStorage.js
+++ Scripts/localStorage.js
@@ -1,12 +1,27 @@
-// get items from weather page
-const searchFavorite = document.querySelector("#addToFav");
+let locationsList = [];
+
+// GETTING ITEMS FROM THE PAGE
+
+
+// city text field
+const locationSearch = document.querySelector("#searchBox");
+
+// favorites button
+const favButton = document.querySelector("#addToFav");
+
+// list of favorites that are from localStorage
 const favoritesList = document.querySelector("#favList");
 
+
+
+// ADDING THINGS TO LOCAL STORAGE
+
+
+// store the city in text field to local storage
+localStorage.setItem("cityLocation", JSON.stringify(locationsList));
+
+// GETTING THINGS FROM LOCAL STORAGE
+
+
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem(""));
-
-// store the new number of visits value
-localStorage.setItem("", numVisits);
-
-// show todays date.
-// todayDisplayDate.textContent = Date.now();
+let listItems = JSON.parse(localStorage.getItem("cityLocation"));
