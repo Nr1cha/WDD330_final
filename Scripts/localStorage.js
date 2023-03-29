6f2b8809393b83bc4a8f67ef7327c989c9c5999e
Nr1cha <vrm.richards@gmail.com>
2023-03-28 20:18:01 -0600

    added to html and corrected value

--- Scripts/localStorage.js
+++ Scripts/localStorage.js
@@ -8,11 +8,15 @@ const locationSearch = document.querySelector("#searchBox");
 // favorites button
 const favButton = document.querySelector("#addToFav");
 
+// adding event listener to a button
 favButton.addEventListener("click", function () {
     // get the value of the item typed
     let searchTerm = locationSearch.value;
-    // Add the search term to the search history array
+    // Add the search term to the locationsList array
     locationsList.push(searchTerm);
+
+    // Log the array to the console
+    console.log(locationsList);
 });
 
 // list of favorites that are from localStorage
